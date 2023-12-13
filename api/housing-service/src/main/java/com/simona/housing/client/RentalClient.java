481d19d4a6910e6f82a8db689c2cf9fbc59232c4
xingcdev <60323726+xingcdev@users.noreply.github.com>
2023-12-13 12:23:00 +0100

    refactor: convert the rental service base url into a variable

--- api/housing-service/src/main/java/com/simona/housing/client/RentalClient.java
+++ api/housing-service/src/main/java/com/simona/housing/client/RentalClient.java
@@ -10,8 +10,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
-@FeignClient(name = "rental-service", url = "/rental/v1")
+@FeignClient(name = "rental-service", url = "${rental-service.base-url}")
 public interface RentalClient {
+
     @GetMapping("/rentals/housing/{housingId}")
     ApiResponse<List<RentalDto>> getRentalsByHousingId(@RequestHeader("Authorization") String authorization, @PathVariable Long housingId, @RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "20") int size,
                                                        @RequestParam(defaultValue = "id") String sortBy);
